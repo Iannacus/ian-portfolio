dbe444f4ada3a161488708b6ee0a1ac2d440d307
Ian Rosas <ian.rosasmal@gmail.com>
2021-06-18 08:31:10 -0500

    se arregla el tamaño del fondo y evita que ese realice scroll al terminar la animacion

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -3,12 +3,12 @@ import Bubble from './Bubble';
 
 function Bubbles() {
     return (
-        <>
+        <div className='bubbles_area'>
             <Bubble pos={'bubble1'} />
             <Bubble pos={'bubble2'} />
             <Bubble pos={'bubble3'} />
             <Bubble pos={'bubble4'} />
-        </>
+        </div>
     )
 }
 
