23dce9ceaa19672b1faac4dbf09632ededc11118
Ian Rosas <ian.rosasmal@gmail.com>
2021-06-18 08:11:46 -0500

    secambian enlaces en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ function Header() {
                     <div className='header_logo-txt'>Ian <strong>Rosas</strong></div>
                 </div>
                 <nav>
-                    <a className='gray' href='#'>About Me</a>
-                    <a className='gray' href='#'>Resume</a>
-                    <a className='gray' href='#'>Portfolio</a>
-                    <a className='gray' href='#'>Contac Me</a>
+                    <a className='gray' href='https://ianrosas.netlify.app'>About Me</a>
+                    <a className='gray' href='https://ianrosas.netlify.app'>Resume</a>
+                    <a className='gray' href='https://ianrosas.netlify.app'>Portfolio</a>
+                    <a className='gray' href='https://ianrosas.netlify.app'>Contac Me</a>
                 </nav>
             </div>
         </header>
